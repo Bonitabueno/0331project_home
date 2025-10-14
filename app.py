17edacfd75a69b268360f35ec7f045a2d4db110f
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-14 20:44:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import traceback
 from admin_module.allowed_admin import ALLOWED_ADMINS_0331
 
 # Streamlit 페이지 설정
-st.set_page_config(page_title="0331 Project", layout="centered", page_icon="📊")
+st.set_page_config(page_title="Project 0331", layout="centered", page_icon="📊")
 
 # 허용된 관리자
 ALLOWED_ADMINS = ALLOWED_ADMINS_0331
@@ -21,7 +21,7 @@ if st.session_state["admin_id"] is None:
         if admin_input in ALLOWED_ADMINS:
             st.session_state["admin_id"] = admin_input
             try:
-                st.switch_page("pages/page_admin_main.py")
+                st.switch_page("dashboard.py")
             except Exception as e:
                 st.code(traceback.format_exc())
         else:
