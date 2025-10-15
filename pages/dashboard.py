4228353887761d17d7f5ad476e21d7717406f058
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-15 19:09:37 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -5,14 +5,14 @@ col1, col2 = st.columns(2)
 
 # 첫 번째 컬럼 안에 컨테이너
 with col1:
-    container1 = st.container()
+    container1 = st.container(border=True)
     with container1:
         if st.button("컬럼 1 버튼"):
             st.write("컬럼 1 버튼 클릭됨")
 
 # 두 번째 컬럼 안에 컨테이너
 with col2:
-    container2 = st.container()
+    container2 = st.container(border=True)
     with container2:
         if st.button("컬럼 2 버튼"):
             st.write("컬럼 2 버튼 클릭됨")
