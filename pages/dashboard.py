979148e33a902cd50a67458681555c1fb8fbcce1
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-15 19:12:58 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
+container = st.container(border=True)
+
 # 컬럼 생성 : 현재 2개
 col1, col2 = st.columns(2)
 
-# 첫 번째 컬럼 안에 컨테이너
 with col1:
     container1 = st.container(border=True)
     with container1:
-        if st.button("컬럼 1 버튼"):
+        if st.button("팝업라이브"):
             st.write("컬럼 1 버튼 클릭됨")
 
-# 두 번째 컬럼 안에 컨테이너
 with col2:
     container2 = st.container(border=True)
     with container2:
-        if st.button("컬럼 2 버튼"):
+        if st.button("포스트26"):
             st.write("컬럼 2 버튼 클릭됨")
