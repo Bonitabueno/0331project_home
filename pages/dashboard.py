d57013f0246cf064892348f3f20e5c2601aa10d5
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-16 19:09:35 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -39,7 +39,7 @@ with col1:
     container1 = st.container(border=True)
     with container1:
         if st.button("팝업라이브"):
-            st.write("컬럼 1 버튼 클릭됨")
+            st.switch_page("pages/popuplive_main.py")
 
 with col2:
     container2 = st.container(border=True)
