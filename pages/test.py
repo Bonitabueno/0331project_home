5808456b04cbe04ae97721e727e52b4bcf6a733e
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-16 20:39:47 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -11,6 +11,5 @@ edited_json = st.text_area("🧾 JSON 편집", json_str, height=300)
 try:
     parsed_data = json.loads(edited_json)
     st.success("✅ JSON 구문 오류 없음")
-    st.json(parsed_data)
 except json.JSONDecodeError:
     st.error("❌ JSON 형식 오류가 있습니다.")
