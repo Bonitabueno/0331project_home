2aad070a1710ee9cb3c37567c466a4b0894b441e
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-18 15:43:42 +0900

    Update popuplive_main.py

--- pages/popuplive_main.py
+++ pages/popuplive_main.py
@@ -5,6 +5,8 @@ import pandas as pd
 # Streamlit 페이지 설정
 st.set_page_config(page_title="0331 Project", layout="wide", page_icon="📊")
 
+
+
 # JSON 불러오기
 url = "https://raw.githubusercontent.com/Bonitabueno/0331project/refs/heads/main/popup_store.json"
 data = requests.get(url).json()
@@ -16,5 +18,5 @@ df.index = df.index + 1
 df.index.name = "" # 인덱스 열 이름 지정
 
 # Streamlit 표시
-st.header("📋 팝업스토어 리스트")
+st.subheader("📋 팝업스토어 리스트")
 st.dataframe(df, use_container_width=True)
