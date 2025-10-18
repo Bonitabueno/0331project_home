c5e2e814ec9449cb86611f4eca5bfbc153d4fa6a
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-18 16:39:44 +0900

    Update and rename cookie_management.py to login_management.py

--- admin_module/login_management.py
+++ admin_module/login_management.py
@@ -7,3 +7,12 @@ def init_cookies(prefix="0331_admin_", password="my_secret_password_0331"):
     if not cookies.ready():
         st.stop()
     return cookies
+
+# 로그인 체크
+def check_login(cookies):
+    admin_id = st.session_state.get("admin_id") or cookies.get("admin_id")
+    if not admin_id:
+        st.warning("로그인이 필요합니다.")
+        st.switch_page("app.py")
+    st.session_state["admin_id"] = admin_id
+    return admin_id
