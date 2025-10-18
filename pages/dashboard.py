9869e804c35515540991d7b7c9fb3e20f579bd48
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-10-18 16:42:41 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,35 +1,22 @@
 import streamlit as st
 from streamlit_cookies_manager import EncryptedCookieManager
-from admin_module.cookie_management import init_cookies
+from admin_module.login_management import init_cookies
+from admin_module.login_management import check_login
 
 # Streamlit 페이지 설정
 st.set_page_config(page_title="0331 Project", layout="centered", page_icon="📊")
 
-# 쿠키 매니저 설정
-cookies = EncryptedCookieManager(
-    prefix="0331_admin_",
-    password="my_secret_password_0331"
-)
-if not cookies.ready():
-    st.stop()
-    
-# 세션에 저장된 admin_id 불러오기
-admin_id = st.session_state.get("admin_id") or cookies.get("admin_id")
+cookies = init_cookies()
+admin_id = check_login(cookies)
 
-# 로그인 유지
-if admin_id:
-    st.session_state["admin_id"] = admin_id
 
-    # 컨테이너 (환영 문구 + 로그아웃 버튼)
-    container = st.container(border=True)
-    container.write(f"{admin_id}님 환영합니다.")
-    if container.button("로그아웃"):
-        cookies["admin_id"] = ""
-        cookies.save()
-        st.session_state["admin_id"] = None
-        st.switch_page("app.py")
-else:
-    st.warning("로그인이 필요합니다.")
+# 사용자 컨테이너 (문구 + 로그아웃 버튼)
+container = st.container(border=True)
+container.write(f"{admin_id}님 환영합니다.")
+if container.button("로그아웃"):
+    cookies["admin_id"] = ""
+    cookies.save()
+    st.session_state["admin_id"] = None
     st.switch_page("app.py")
 
 # 구분선
