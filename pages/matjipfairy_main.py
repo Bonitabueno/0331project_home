f9c5d2b54e51d2545fa077e03e24c8f42b337579
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-11-28 19:32:25 +0900

    Update matjipfairy_main.py

--- pages/matjipfairy_main.py
+++ pages/matjipfairy_main.py
@@ -12,4 +12,19 @@ st.set_page_config(page_title="0331 Project", layout="wide", page_icon="📊")
 cookies = init_cookies()
 admin_id = check_login(cookies)
 
+# 사용자 컨테이너 (문구 + 로그아웃 버튼)
+with st.container(border=True):
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        if st.button("홈"):
+            st.switch_page("pages/home.py")
+
+    with col2:
+        if st.button("로그아웃"):
+            cookies["admin_id"] = ""
+            cookies.save()
+            st.session_state["admin_id"] = None
+            st.switch_page("app.py")
+
 st.write("페이지 준비중입니다.")
