40d76c3a35501ba0d49646d5488d0191cf54cc3f
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2025-12-03 21:33:53 +0900

    Update matjipfairy_global_upload.py

--- pages/matjipfairy_global_upload.py
+++ pages/matjipfairy_global_upload.py
@@ -41,7 +41,7 @@ with tab1:
     # ============================
     # 데이터 확인 버튼
     # ============================
-    if st.button("데이터 확인"):
+    if st.button("데이터 확인", key="Data_check"):
         fixed_address = address.replace(" District", "-gu")
 
         st.session_state.matjip_data = {
